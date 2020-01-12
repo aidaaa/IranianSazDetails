ac92d97d2538aec150198f82360cdbb002244c72
Aida <a.mosayebi478@gmail.com>
2020-01-12 17:11:49 +0330

    Full Screen

--- app/src/main/java/com/example/iraniansazdetails/MainActivity.java
+++ app/src/main/java/com/example/iraniansazdetails/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -47,63 +48,39 @@ public class MainActivity extends FragmentActivity {
         viewPager.setAdapter(adapter);
 
         tabLayout.setupWithViewPager(viewPager);
-        tabLayout.getTabAt(0).setIcon(R.drawable.headphones);
+
+        if ((getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+        {
+            tabLayout.getTabAt(0).setIcon(R.drawable.nheadphones);
+            tabLayout.getTabAt(1).setIcon(R.drawable.ninformation2);
+            tabLayout.getTabAt(2).setIcon(R.drawable.npicture2);
+            tabLayout.getTabAt(3).setIcon(R.drawable.nvideo2);
+        }
+        if ((getResources().getConfiguration().screenLayout &
+                    Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                    Configuration.SCREENLAYOUT_SIZE_LARGE)
+            {
+                tabLayout.getTabAt(0).setIcon(R.drawable.lheadphones);
+                tabLayout.getTabAt(1).setIcon(R.drawable.linformation2);
+                tabLayout.getTabAt(2).setIcon(R.drawable.lpicture2);
+                tabLayout.getTabAt(3).setIcon(R.drawable.lvideo2);
+            }
+        if ((getResources().getConfiguration().screenLayout &
+                        Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                        Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                {
+                    tabLayout.getTabAt(0).setIcon(R.drawable.xheadphones);
+                    tabLayout.getTabAt(1).setIcon(R.drawable.xinformation2);
+                    tabLayout.getTabAt(2).setIcon(R.drawable.xpicture2);
+                    tabLayout.getTabAt(3).setIcon(R.drawable.xvideo2);
+                }
+   /*     tabLayout.getTabAt(0).setIcon(R.drawable.headphones);
         tabLayout.getTabAt(1).setIcon(R.drawable.information2);
         tabLayout.getTabAt(2).setIcon(R.drawable.picture2);
-        tabLayout.getTabAt(3).setIcon(R.drawable.video2);
+        tabLayout.getTabAt(3).setIcon(R.drawable.video2);*/
 
-
-    /*    AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AlertDialogStyle);
-
-        builder.setTitle("اطلاعات");
-        builder.setIcon(R.drawable.teh);
-        builder.setMessage(getCityInfo());
-
-         *//*   builder.setPositiveButton("بله", new DialogInterface.OnClickListener() {
-                @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-                public void onClick(DialogInterface dialog, int which) {
-                    finishAffinity();
-                }
-            });*//*
-
-        builder.setNegativeButton("باشه", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        AlertDialog alert = builder.create();
-        alert.show();
-        alert.getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);*/
-
-
-
-       /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-       final AlertDialog dialog = builder.create();
-       LayoutInflater inflater = getLayoutInflater();
-       View dialogLayout = inflater.inflate(R.layout.text_dialog, null);
-       dialog.setView(dialogLayout);
-       dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
-
-       dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-           @Override
-           public void onShow(DialogInterface d) {
-               TextView txt =dialog.findViewById(R.id.dialogText);
-               txt.setText(getCityInfo());
-
-               Typeface typface=Typeface.createFromAsset(getAssets(),"fonts/iransans.ttf");
-               txt.setTypeface(typface);
-               txt.setTypeface(typface);
-
-               //  LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(Math.round(imageWidthInPX),
-               //          Math.round(imageWidthInPX * (float)image.getHeight() / (float)image.getWidth()));
-               //  image.setLayoutParams(layoutParams);
-           }
-       });
-       dialog.show();*/
 
         setCustomFont();
 
@@ -114,19 +91,94 @@ public class MainActivity extends FragmentActivity {
                 switch (i)
                 {
                     case 0:
-                        tabLayout.getTabAt(0).setIcon(R.drawable.headphones);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.nheadphones);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.lheadphones);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.xheadphones);
+                        }
+                       // tabLayout.getTabAt(0).setIcon(R.drawable.headphones);
                         setCustomFont();
                         break;
+
                     case 1:
-                        tabLayout.getTabAt(1).setIcon(R.drawable.information);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.ninformation);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.linformation);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.xinformation);
+                        }
+                        //tabLayout.getTabAt(1).setIcon(R.drawable.information);
                         setCustomFont();
                         break;
+
                     case 2:
-                        tabLayout.getTabAt(2).setIcon(R.drawable.picture);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.npicture);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.lpicture);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.xpicture);
+                        }
+                        //tabLayout.getTabAt(2).setIcon(R.drawable.picture);
                         setCustomFont();
                         break;
+
                     case 3:
-                        tabLayout.getTabAt(3).setIcon(R.drawable.video);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.nvideo);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.lvideo);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.xvideo);
+                        }
+                        //tabLayout.getTabAt(3).setIcon(R.drawable.video);
                         setCustomFont();
                         break;
                 }
@@ -139,19 +191,94 @@ public class MainActivity extends FragmentActivity {
                 switch (i)
                 {
                     case 0:
-                        tabLayout.getTabAt(0).setIcon(R.drawable.headphones2);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.nheadphones2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.lheadphones2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(0).setIcon(R.drawable.xheadphones2);
+                        }
+                        // tabLayout.getTabAt(0).setIcon(R.drawable.headphones);
                         setCustomFont();
                         break;
+
                     case 1:
-                        tabLayout.getTabAt(1).setIcon(R.drawable.information2);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.ninformation2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.linformation2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(1).setIcon(R.drawable.xinformation2);
+                        }
+                        //tabLayout.getTabAt(1).setIcon(R.drawable.information);
                         setCustomFont();
                         break;
+
                     case 2:
-                        tabLayout.getTabAt(2).setIcon(R.drawable.picture2);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.npicture2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.lpicture2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(2).setIcon(R.drawable.xpicture2);
+                        }
+                        //tabLayout.getTabAt(2).setIcon(R.drawable.picture);
                         setCustomFont();
                         break;
+
                     case 3:
-                        tabLayout.getTabAt(3).setIcon(R.drawable.video2);
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_NORMAL)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.nvideo2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_LARGE)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.lvideo2);
+                        }
+                        if ((getResources().getConfiguration().screenLayout &
+                                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+                                Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                        {
+                            tabLayout.getTabAt(3).setIcon(R.drawable.xvideo2);
+                        }
+                        //tabLayout.getTabAt(3).setIcon(R.drawable.video);
                         setCustomFont();
                         break;
                 }
@@ -186,53 +313,4 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
-    public String getCityInfo()
-    {
-        String cityInfo="";
-        prefs = this.getSharedPreferences("shared", MODE_PRIVATE);
-        int id=prefs.getInt("id",0);
-        switch (id)
-        {
-            case 0:
-                cityInfo="موسیقی تهران و حومه"+
-                        " \n"+
-                        "بخش اول:موسیقی "+
-                        " \n"+
-                        "شامل 2 موسیقی"+
-                        " \n"+
-                        "بخش دوم:متن مربوط به قوم تهران"+
-                        " \n"+
-                        "بخش سوم:تصاویر"+
-                        " \n"+
-                        "شامل 3 عکس"+
-                        " \n"+
-                        "بخش چهارم:موزیک ویدیو:"+
-                        " \n"+
-                        "شامل 2 موزیک ویدیو";
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-            case 11:
-                break;
-        }
-        return cityInfo;
-    }
 }
